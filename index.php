5b4fe3fb2c61bcec17b52d44c36bd01a54a5a08e
Bagrat Harutyunyan <bagrat@superannotate.com>
2025-05-21 23:57:01 +0400

    add new files

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<body style='background-color:grey'>";
+echo "<h1><font color=blue>Hello From Kubernetes</h1><br>";
+echo "<font color=black>Server IP Address is: $ip_server", "<br><p>";
+echo "<h2><font color=blue>Made for K8s Presentation</h2>";
+echo "<h2><font color=green> Version 2 </h2>";
+?>
